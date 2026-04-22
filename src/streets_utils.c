24cb04b3a10ef30c7f008b99991b98ad0cda137f
fridadonate01-prog <frida.donate01@estudiant.upf.edu>
2026-04-22 19:46:06 +0200

    Changed the streets_utils.c

--- src/streets_utils.c
+++ src/streets_utils.c
@@ -5,7 +5,16 @@ void normalizeStreetName(char* destination, const char* original) {
     if (strncmp(original, "C. ", 3) == 0 || strncmp(original, "C/ ", 3) == 0) {
         strcpy(destination, "Carrer ");
         strcat(destination, original + 3);
-    } else {
+    }else if(strncmp(original, "Av. ", 4) == 0 || strncmp(original, "Av/ ", 4) == 0){
+        strcpy(destination, "Avinguda ");
+        strcat(destination, original + 4);
+    }else if(strncmp(original, "Pass. ", 6) == 0 || strncmp(original, "Pass/ ", 6) == 0){
+        strcpy(destination, "Passeig ");
+        strcat(destination, original + 6);
+    }else {
         strcpy(destination, original);
     }
 }
+
+
+// Avinguda (Av. o Av/), Passeig(Pass. o Pass/)
