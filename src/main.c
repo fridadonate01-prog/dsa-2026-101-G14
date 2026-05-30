9123d0d69ebc30e940d2f2a1943256587b5f5dcb
fridadonate01-prog <frida.donate01@estudiant.upf.edu>
2026-05-30 15:52:18 +0200

    Added the clock timer for the report

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <time.h>
 #include "find.h"
 #include "route.h"
 
@@ -67,8 +68,20 @@ int main() {
   // GENERATE AND PRINT THE ROUTE
   // If both an origin and destination were succesfully found, run the BFS
   if (origin_street != NULL && dest_street != NULL) {
-    PathNode* final_route = find_route(origin_street, dest_street, all_streets);
 
+    // Start Timer
+    clock_t start = clocl();
+
+    StreetNode* final_route = find_route(origin_street, dest_street, all_streets);
+
+    // Stop Timer
+    clock_t end = clock();
+
+    // Calculate latency in miliseconds
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC * 1000.0;
+    printf("\nLatency Results for map '%s' :\n", all_maps[chosen_map]);
+    printf("Time taken: %f ms\n", time_spent);
+    
     print_route_directions(final_route);
 
     free_path(final_route);
